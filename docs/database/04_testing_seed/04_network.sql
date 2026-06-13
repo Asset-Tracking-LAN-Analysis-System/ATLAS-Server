0e02e115546c072efd4d8c499f33e52c9a5bfc11
Veicm <180028722+Veicm@users.noreply.github.com>
2026-06-13 14:32:53 +0200

    Add initial seed data for network interfaces and links

---
+++ docs/database/04_testing_seed/04_network.sql
@@ -0,0 +1,6 @@
+INSERT INTO network_interfaces VALUES(0, '003-000000', 'Omega-eth0', 'aa:bb:cc:dd:ee:00', '192.168.734.34');
+
+INSERT INTO network_interfaces VALUES(1, '003-000001', 'Bravo-eth0', 'aa:bb:cc:dd:ee:01', '192.168.734.79');
+
+
+INSERT INTO network_links VALUES(0, 0, 1);
